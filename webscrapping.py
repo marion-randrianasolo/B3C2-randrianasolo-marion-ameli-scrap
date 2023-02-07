946a9e18c26642fc146dbc51869d3264993afead
Marion Randrianasolo <marion.randrianasolo@etu.umontpellier.fr>
2023-02-07 12:42:40 +0100

    récupération classes

--- webscrapping.py
+++ webscrapping.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+import pandas as pd
 
 url = 'http://www.imdb.com/chart/top'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
+films = soup.find_all("td", class_="titleColumn")
+notes = soup.find_all("td", class_="imdbRating")
+
